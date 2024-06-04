518df3d67f002fde1f245515679e127831eb9080
samstickkz <samstickkz@gmail.com>
2024-06-04 19:48:21 +0100

    nav1

--- vite-project/src/components/navbar/navbar/navbar.jsx
+++ vite-project/src/components/navbar/navbar/navbar.jsx
@@ -1,7 +1,24 @@
 import "./navbar.css";
+// import logo from "../../../assets/logo.jpeg";
 
-const navbar = () => {
-  return <div className="navbar">navbar</div>;
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <small className="logo">Samuel</small>
+      <ul>
+        <li>home</li>
+        <li>Features</li> <li>pricing</li> <li>About</li>
+      </ul>
+      <div className="nav-right">
+        <select>
+          <option value="usd">NGN</option>
+          <option value="usd">USD</option> <option value="usd">USD</option>{" "}
+          <option value="usd">USD</option> <option value="usd">USD</option>
+        </select>
+        <button>Sign up</button>
+      </div>
+    </div>
+  );
 };
 
-export default navbar;
+export default Navbar;
