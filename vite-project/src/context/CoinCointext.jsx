e456fa370d5204b57482f8987b2b1d153e3c53bf
samstickkz <samstickkz@gmail.com>
2024-06-05 07:43:49 +0100

    api f

--- vite-project/src/context/CoinCointext.jsx
+++ vite-project/src/context/CoinCointext.jsx
@@ -1,14 +1,48 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-export const CoinContext = createContext;
+export const CoinContext = createContext();
 
 const CoinContextProvider = (props) => {
-  const contextValue = {};
+  const [allCoins, setAllCoins] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "usd",
+    symbol: "$",
+  });
+
+  useEffect(() => {
+    // Function to fetch coin data from CoinGecko API
+    const fetchCoins = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          "x-cg-demo-api-key": "CG-4gnqjEX2eVtmTgpdwgnS7ycH	",
+        },
+      };
+
+      fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => setAllCoins(response))
+        .catch((err) => console.error(err));
+    };
+
+    fetchCoins();
+  }, [currency]);
+
+  const contextValue = {
+    allCoins,
+    currency,
+    setCurrency,
+  };
+
   return (
-    <CoinContext.provider value={contextValue}>
+    <CoinContext.Provider value={contextValue}>
       {props.children}
-    </CoinContext.provider>
+    </CoinContext.Provider>
   );
 };
 
